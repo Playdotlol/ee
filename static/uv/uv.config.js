0d45e7c4c64be9d1997c71d8887af463430af96a
Playdotlol <dexxandshizz@outlook.com>
2025-06-16 11:40:21 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://loungef2x.koyeb.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: 'https://playdotlol.github.io/ee/static/uv/uv.handler.js',
+    bundle: 'https://playdotlol.github.io/ee/static/uv/uv.bundle.js',
+    config: 'https://playdotlol.github.io/ee/static/uv/uv.config.js',
+    sw: 'https://playdotlol.github.io/ee/static/uv/uv.sw.js',
 };
