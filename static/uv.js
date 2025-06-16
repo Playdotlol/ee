00475c6cfa926df0d307d358d1661dd062b4134f
Playdotlol <dexxandshizz@outlook.com>
2025-06-16 11:37:23 -0400

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('https://playdotlol.github.io/ee/static/uv/uv.bundle.js');
+importScripts('https://playdotlol.github.io/ee/static/uv/uv.config.js');
+importScripts('https://playdotlol.github.io/ee/static/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
